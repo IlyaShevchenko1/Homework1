a67fdd86402e57592664cc1a2b6a0dcbe77a0bb2
Ilya Shevchenko <6BLK@mail.ru>
2023-07-24 13:23:57 +0300

    Task 2 was added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// C# Practice. First homework.
+
+// Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+/*
+Console.Write("Input a first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine($"First number: {firstNumber} is bigger than second number: {secondNumber}");
+}
+else if (firstNumber < secondNumber)
+{
+    Console.WriteLine($"First number: {firstNumber} is less than second number: {secondNumber}");
+}
+else
+{
+    Console.WriteLine($"First number: {firstNumber} is equal to second number: {secondNumber}");
+}
+*/
+
